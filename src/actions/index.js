bdd95028a42042521d169d4d3fc284fc76c2854e
Garan Guillory <garanguillory@gmail.com>
2016-08-07 15:35:55 -0500

    weather forecast app in progress

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = '71019c97455e432901d8c2413b8eeaa4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url); // will return a promise
+
+	console.log('Request: ', request);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request 
+	};
+}
+
